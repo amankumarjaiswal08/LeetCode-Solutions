7f5f6d75dbbd74beaf711b6e3e3889fcccc5ecce
Aman Kumar Jaiswal <2k24.cs1c.2413983@gmail.com>
2026-09-09 12:44:30 +0530

    Time: 0 ms (100%), Space: 8.5 MB (51.72%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     int countCommas(int n) {
-        if(n<1000) return 0;
-        int count = 0 ;
-        long long factor = 1000;
-        while(n>=factor){
-            count += (n-factor+1);
-            factor *= 1000;
-        }
-        return count;
+        return max(n-999 , 0);
     }
 };
